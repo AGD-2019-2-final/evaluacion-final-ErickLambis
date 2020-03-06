055eb81fb2ad25300bac6a869a4d81187fef51f6
ErickLambis <57542392+ErickLambis@users.noreply.github.com>
2020-03-06 15:41:05 -0500

    Update mapper.py

--- 01-hadoop-50/q09-10/mapper.py
+++ 01-hadoop-50/q09-10/mapper.py
@@ -2,3 +2,9 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for i in sys.stdin:
+      key = i.split('   ')[0]
+      valcol1 = i.split('   ')[1]
+      valcol2 = i.split('   ')[2].strip()
+      sys.stdout.write("{}\t{}\t{}\n".format(key,valcol1,valcol2))
