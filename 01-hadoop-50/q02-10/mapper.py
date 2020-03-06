47d0efbe059aa21ce6d6e219041772744879d531
ErickLambis <57542392+ErickLambis@users.noreply.github.com>
2020-03-06 15:36:51 -0500

    Update mapper.py

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,10 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    
+    for line in sys.stdin:
+
+        amount = line.split(',')
+        sys.stdout.write("{}\t{}\n".format(amount[3],amount[4]))
